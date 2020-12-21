c5f7a0169d5cd3e882db842108756882c7166140
edvinsjogren <edvin.sjogren@gmail.com>
2020-12-21 16:25:46 +0100

    first commit contacts

--- JS/contact.js
+++ JS/contact.js
@@ -1,23 +1,181 @@
 $(function() {
 
-    $( ".hamburgerButton" ).click(function() {
-        $( " .myUl " ).slideToggle();
-        $( ".myUl" ).css({
-            display: "flex"
-        })
-        console.log(("klick"));
-        });
-        function getBag() {
-            bag = JSON.parse(localStorage.getItem("products"));
-            }
-        const cartNumbers = () => {
-            getBag();
-            let itemsInCart = 0;
-            for (let i = 0; i < bag.length; i++) {
-              itemsInCart += bag[i].quantity;
-            }
-            return itemsInCart;
-          };
-    
-        document.querySelector('.shoppingCartButton i').textContent = cartNumbers();
-    });
+  $( ".hamburgerButton" ).click(function() {
+  
+    $( " .myUl " ).slideToggle();
+    $( ".myUl" ).css({
+      display: "flex"
+    })
+  });
+
+function getBag() {
+  bag = JSON.parse(localStorage.getItem("products"));
+}
+
+const cartNumbers = () => {
+  getBag();
+  let itemsInCart = 0;
+
+  for (let i = 0; i < bag.length; i++) {
+    itemsInCart += bag[i].quantity;
+  }
+  return itemsInCart;
+};
+
+document.querySelector('.shoppingCartButton i').textContent = cartNumbers();
+renderHTML();
+});
+
+function renderHTML(){
+
+  $("<h1>")
+  .html("LOGOTYPE")
+  .appendTo("main")
+
+  $("<img>")
+  .attr("src", "../IMG/iceman.jpeg")
+  .appendTo("main")
+
+  let container = $("<div>")
+  .attr("id", "contactContainer")
+  .appendTo("main")
+
+  $("<h2>")
+  .html("contact")
+  .appendTo(container)
+
+  $("<h3>")
+  .html("official retailers world wide:")
+  .appendTo(container)
+
+  $("<h4>")
+  .html("US")
+  .appendTo(container)
+
+  $("<p>")
+  .html("us@company.com")
+  .appendTo(container)
+
+  $("<h4>")
+  .html("SWEDEN")
+  .appendTo(container)
+
+  $("<p>")
+  .html("se@company.com")
+  .appendTo(container)
+
+  $("<h4>")
+  .html("UK")
+  .appendTo(container)
+
+  $("<p>")
+  .html("uk@company.com")
+  .appendTo(container)
+
+  $("<h4>")
+  .html("FRANCE")
+  .appendTo(container)
+
+  $("<p>")
+  .html("fr@company.com")
+  .appendTo(container)
+
+  $("<h4>")
+  .html("JAPAN")
+  .appendTo(container)
+
+  $("<p>")
+  .html("jp@company.com")
+  .appendTo(container)
+
+  $("<h4>")
+  .html("DENMARK")
+  .appendTo(container)
+
+  $("<p>")
+  .html("dk@company.com")
+  .appendTo(container)
+
+  $("<h4>")
+  .html("AUSTRALIA")
+  .appendTo(container)
+
+  $("<p>")
+  .html("au@company.com")
+  .appendTo(container)
+
+  $("<h4>")
+  .html("GENERAL INQUIRIES")
+  .appendTo(container)
+
+  $("<p>")
+  .html("info@company.com")
+  .appendTo(container)
+  
+  $("<h4>")
+  .html("PRESS")
+  .appendTo(container)
+
+  $("<p>")
+  .html("press@company.com")
+  .appendTo(container)
+
+  let postalAddress = $("<div>")
+  .addClass("sectionContainer")
+  .appendTo(container)
+
+  $("<h4>")
+  .html("POSTAL ADDRESS")
+  .appendTo(postalAddress)
+
+  $("<p>")
+  .html("Company Name")
+  .appendTo(postalAddress)
+
+  $("<p>")
+  .html("Box 1234")
+  .appendTo(postalAddress)
+
+  $("<p>")
+  .html("S-123 45 Stockholm")
+  .appendTo(postalAddress)
+
+  $("<p>")
+  .html("Sweden")
+  .appendTo(postalAddress)
+
+  $("<h4>")
+  .html("PHONE")
+  .appendTo(container)
+
+  $("<p>")
+  .html("+46(0)70-123 45 67")
+  .appendTo(container)
+
+  let followContainer = $("<div>")
+  .addClass("sectionContainer")
+  .appendTo(container)
+
+  $("<h4>")
+  .html("FOLLOW")
+  .appendTo(followContainer)
+
+  $("<p>")
+  .html("instagram.com/company")
+  .appendTo(followContainer)
+  $("<p>")
+  .html("twitter.com/company")
+  .appendTo(followContainer)
+  $("<p>")
+  .html("facebook.com/company")
+  .appendTo(followContainer)
+
+  $("<h4>")
+  .html("WEBSITE BY")
+  .appendTo(container)
+
+  $("<p>")
+  .html("Group 4 - FED20S @ Medieinstitutet")
+  .appendTo(container)
+}
+
