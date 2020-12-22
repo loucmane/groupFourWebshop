72802a26b6780d0a6763f7e646dbcedc92d8cb17
edvinsjogren <edvin.sjogren@gmail.com>
2020-12-22 15:43:47 +0100

    fixed cartbuttons with new class

--- JS/checkout.js
+++ JS/checkout.js
@@ -1,37 +1,12 @@
-class Product {
-    constructor(image, name, price, description, id, quantity) {
-        this.image = image;
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.id = id;
-        this.quantity = quantity;
+class CustomerInfo {
+    constructor(email, firstName, lastName) {
+        
     }
 }
 
 let bag = [];
 
 let custInfo = [];
-
-// Save shopping cart in LS
-function saveBag() {
-    localStorage.setItem("products", JSON.stringify(bag));
-}
-
-// Get shopping cart from LS
-function getBag() {
-    bag = JSON.parse(localStorage.getItem("products"));
-}
-
-// Save customer information in LS
-function saveCustInfo() {
-    localStorage.setItem("Information", JSON.stringify(custInfo));
-}
-
-// Get customer information from LS
-function getCustInfo() {
-    custInfo = JSON.parse(localStorage.getItem("Information"));
-}
 
 $(function() {
 
@@ -45,71 +20,71 @@ $(function() {
             window.location.href = "#payment";
         });
 
-
     $("#btnReviewOrder")
         .on("click", function() {
             $("<p>").appendTo($("#transferredCustPayment"))
-                .html("Contact" +
-                    " " +
-                    $("#email").val());
+            .html("Contact" +
+            " " +
+            $("#email").val());
+
+            $("<hr>")
+            .appendTo($("#transferredCustPayment"));
+
+            $("<p>")
+            .appendTo($("#transferredCustPayment"))
+            .html("Ship to " +
+            $("#fName").val() +
+            " " +
+            $("#lName").val() +
+            ", " +
+            $("#adrStreet").val() +
+            ", " +
+            $("#adrPCode").val() +
+            " " +
+            $("#adrCity").val() +
+            ", " +
+            $("#adrCountry").val());
 
             $("<hr>")
                 .appendTo($("#transferredCustPayment"));
 
             $("<p>")
-                .appendTo($("#transferredCustPayment"))
-                .html("Ship to " +
-                    $("#fName").val() +
-                    " " +
-                    $("#lName").val() +
-                    ", " +
-                    $("#adrStreet").val() +
-                    ", " +
-                    $("#adrPCode").val() +
-                    " " +
-                    $("#adrCity").val() +
-                    ", " +
-                    $("#adrCountry").val());
-
-            $("<hr>")
-                .appendTo($("#transferredCustPayment"));
+            .appendTo($("#transferredCustPayment"))
+            .html("Shipping Postnord 59 SEK");
 
             $("<p>")
-                .appendTo($("#transferredCustPayment"))
-                .html("Shipping Postnord 59 SEK");
-
-            $("<p>")
-                .appendTo($("#transferredCustPayment"))
-                .html("Payment Nets Payment");
+            .appendTo($("#transferredCustPayment"))
+            .html("Payment Nets Payment");
 
             $("<button>")
-                .appendTo($("#transferredCustPayment"))
-                .html("Change")
-                .on("click", function() {});
+            .appendTo($("#transferredCustPayment"))
+            .html("Change")
+            .on("click", function() {});
 
             $("<p>")
-                .appendTo($("#checkoutTotal"))
-                .html("Subtotal " +
-                    costfromLS() +
-                    " SEK");
+            .appendTo($("#checkoutTotal"))
+            .html("Subtotal " +
+                costfromLS() +
+                " SEK");
 
             $("<p>")
-                .appendTo($("#checkoutTotal"))
-                .html("Shipping 59 SEK");
+            .appendTo($("#checkoutTotal"))
+            .html("Shipping 59 SEK");
 
             $("<hr>")
-                .appendTo($("#checkoutTotal"));
+            .appendTo($("#checkoutTotal"));
 
             $("<p>")
-                .appendTo($("#checkoutTotal"))
-                .addClass("total")
-                .html("Total " +
-                    grandTotal() +
-                    " SEK");
+            .appendTo($("#checkoutTotal"))
+            .addClass("total")
+            .html("Total " +
+                grandTotal() +
+                " SEK");
 
             window.location.href = "#review";
 
-
+            custInfo.push();
+            saveBag();
         });
 
     $("#completeButton")
@@ -117,8 +92,6 @@ $(function() {
 
             window.location.href = "../HTML/confirmation.html";
         });
-
-
 });
 
 function costfromLS(params) {
