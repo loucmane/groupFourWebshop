a2dd6974421fe8487e9d4801b2b6700e874fb28f
edvinsjogren <edvin.sjogren@gmail.com>
2020-12-22 18:10:57 +0100

    checkout update new Class

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,63 @@
 $(function() {
 
-  $( ".shoppingCartButton").click(function(){
-    window.location.href = "../HTML/cart.html";
 });
 
-  $( ".logotype").click(function(){
-    window.location.href = "../index.html";
-    console.log("klick");
-  });
+class Product {
+  constructor(image, name, price, description, id) {
+      this.image = image;
+      this.name = name;
+      this.price = price;
+      this.description = description;
+      this.id = id;
+  }
+}
 
+class CartItem {
+  constructor(product, quantity) {
+      this.product = product;
+      this.quantity = quantity;
+  }
+}
+
+class CustomerInfo {
+  constructor(email, fName, lName, street, postal, city, country, phone, shipping) {
+      this.email = email;
+      this.fName = fName;
+      this.lName = lName;
+      this.street = street;
+      this.postal = postal;
+      this.city = city;
+      this.country = country;
+      this.phone = phone;
+      this.shipping = shipping;
+  }
+}
+
+let product1 = new Product("../IMG/perfume-bottle-bergamot.jpg", "Bergamot", 850, "Lorem Ipsum", 1, 0);
+let product2 = new Product("../IMG/perfume-bottle-iris.jpg", "Iris", 850, "Lorem Ipsum", 2, 0);
+let product3 = new Product("../IMG/perfume-bottle-saffron.jpg", "Saffron", 850, "Lorem Ipsum", 3, 0);
+let product4 = new Product("../IMG/perfume-bottle-sandalwood.jpg", "Sandalwood", 850, "Lorem Ipsum", 4, 0);
+let product5 = new Product("../IMG/perfume-bottle-ginger.jpg", "Ginger", 850, "Lorem Ipsum", 5, 0);
+
+myProducts = [product1, product2, product3, product4, product5];
+bag = [];
+custInfo = [];
+
+function setToLocalStorage() {
+  localStorage.setItem("products", JSON.stringify(bag));
+  localStorage.setItem("Information", JSON.stringify(custInfo));
+}
+
+function getFromLocalStorage() {
+  bag = JSON.parse(localStorage.getItem("products")) || [] ;
+  custInfo = JSON.parse(localStorage.getItem("Information")) || [] ;
+}
+
+$( ".shoppingCartButton").click(function(){
+  window.location.href = "../HTML/cart.html";
 });
 
+$( ".logotype").click(function(){
+  window.location.href = "../index.html";
+  console.log("klick");
+});
--- JS/products.js
+++ JS/products.js
@@ -1,35 +1,6 @@
-class Product {
-    constructor(image, name, price, description, id) {
-        this.image = image;
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.id = id;
-    }
-}
-
-class CartItem {
-    constructor(product, quantity) {
-        this.product = product;
-        this.quantity = quantity;
-    }
-}
-
-let description = "Lorem ipsum";
-
-let product1 = new Product("../IMG/perfume-bottle-bergamot.jpg", "Bergamot", 850, description, 1, 0);
-let product2 = new Product("../IMG/perfume-bottle-iris.jpg", "Iris", 850, description, 2, 0);
-let product3 = new Product("../IMG/perfume-bottle-saffron.jpg", "Saffron", 850, description, 3, 0);
-let product4 = new Product("../IMG/perfume-bottle-sandalwood.jpg", "Sandalwood", 850, description, 4, 0);
-let product5 = new Product("../IMG/perfume-bottle-ginger.jpg", "Ginger", 850, description, 5, 0);
-
-let myProducts = [product1, product2, product3, product4, product5];
-
-let bag = [];
-
 $(function() {
     
-    getBag();
+    getFromLocalStorage();
     createBagHTML();
 
     $.each(myProducts, (i, product) => { // Looping the objects and creating html from them //
@@ -72,7 +43,7 @@ $(function() {
                 bag.push(addedItem);
             }
             
-            saveBag();
+            setToLocalStorage();
             createBagHTML();
             cartNumbers();
         });
@@ -98,7 +69,7 @@ function createBagHTML() {
             for (let i = 0; i < bag.length; i++) {
                 if (bag[i].id === product.id) {
                     bag.splice([i], 1);
-                    saveBag();
+                    setToLocalStorage();
                     createBagHTML();
                 }
             }
@@ -115,7 +86,7 @@ function createBagHTML() {
             for (let i = 0; i < bag.length; i++) {
                 if (bag[i].id === product.id) {
                     product.quantity++;
-                    saveBag();
+                    setToLocalStorage();
                     createBagHTML();
                 }
             }
@@ -135,7 +106,7 @@ function createBagHTML() {
                     if (bag[i].quantity === 0) { // If the quantity of the product becomes 0 -> splice that product from array
                         bag.splice([i], 1);
                     }
-                    saveBag();
+                    setToLocalStorage();
                     createBagHTML();
                 }
             }
